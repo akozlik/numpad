a221aead0738540f77c0eb883b26e049dbccc0ab
Your Name <akozlik@gmail.com>
2012-11-27 21:33:21 -0500

    Initial Commit

---
+++ Numpad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Numpad
+//
+//  Created by Andrew on 11/27/12.
+//  Copyright (c) 2012 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
